2ad281f93ecc3b5536354d80b795ba646b04d6c8
Halo0070 <lee998598@gmail.com>
2019-07-17 21:04:53 +0900

    Update BeautifulSoup Crawling.py

--- BeautifulSoup Crawling.py
+++ BeautifulSoup Crawling.py
@@ -3,3 +3,32 @@ import urllib.request
 import json
 import os
 from bs4 import BeautifulSoup
+
+def main(pages):
+    page = 1
+    while page <= pages:
+        url = 'https://www.pinterest.co.kr/pin/755901118681775962/'
+        a_code = requests.get(url)
+        a_text = a_code.text
+        soup = BeautifulSoup(a_text, 'html.parser')
+        for link in soup.findAll('img', ):
+            href = link.get('src')
+            if href == None:
+                print('Nothing')
+                break
+            else:
+                save_image(url,href)
+        page += 1
+ 
+def save_image(fullurl,url):
+    print(fullurl,"   ",url)
+    print(url.find('http')==-1 and url.find('www')==-1)
+    print('->',url)
+    if url.find('http')==-1 and url.find('www')==-1:                #if = -1 (not find)
+        url = fullurl+url
+        print('-<',url)
+    a_code = requests.get(url)
+    pathFileName = os.path.join('D:/GitHub/Python image crawling/Python-image-crawling/image',url[-9:-3]+'.png')
+    urllib.request.urlretrieve(url,pathFileName)
+    
+main(1) #max page
